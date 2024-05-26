4d346f32781798909fd23ea0509e58e4288f9762
timflee <timflee@hotmail.com>
2024-05-26 18:42:08 +1000

    Implemented error checking for string commands too

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 def on_button_pressed_a():
+    global lastSentName, lastSentValue, ACK, lastSentString
+    radio.send_string("ledMapTx")
+    basic.pause(200)
     for index in range(8):
-        radio.send_value(picsA[modes.index(currentMode)][index], index)
+        while not (ACK):
+            lastSentName = picsA[modes.index(currentMode)][index]
+            lastSentValue = index
+            radio.send_value(lastSentName, lastSentValue)
+            basic.pause(100)
+        ACK = False
+    while not (ACK):
+        lastSentString = "updateLeds"
+        radio.send_string(lastSentString)
         basic.pause(100)
-    radio.send_string("updateLeds")
+    ACK = False
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
     radio.send_value("bright", 30)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+def on_received_string(receivedString):
+    global ACK
+    if receivedString == lastSentString:
+        ACK = True
+radio.on_received_string(on_received_string)
+
 def on_button_pressed_b():
+    global lastSentName, lastSentValue, ACK, lastSentString
+    radio.send_string("ledMapTx")
+    basic.pause(200)
     for index2 in range(8):
-        radio.send_value(picsB[modes.index(currentMode)][index2], index2)
+        while not (ACK):
+            lastSentName = picsB[modes.index(currentMode)][index2]
+            lastSentValue = index2
+            radio.send_value(lastSentName, lastSentValue)
+            basic.pause(100)
+        ACK = False
+    while not (ACK):
+        lastSentString = "updateLeds"
+        radio.send_string(lastSentString)
         basic.pause(100)
-    radio.send_string("updateLeds")
+    ACK = False
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_received_value(name, value):
+    global ACK
+    if name == lastSentName and value == lastSentValue:
+        ACK = True
+radio.on_received_value(on_received_value)
 
 def on_logo_pressed():
     global currentMode
@@ -23,7 +57,12 @@ def on_logo_pressed():
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 def initVariables():
-    global modes, currentMode, pic, picsA, picsB
+    global lastSentValue, lastSentName, lastSentString, ACK, messageRx, modes, currentMode, pic, picsA, picsB
+    lastSentValue = 0
+    lastSentName = ""
+    lastSentString = ""
+    ACK = False
+    messageRx = False
     modes = ["E", "S", "Z", "T"]
     currentMode = modes[3]
     pic = ["rrrrrrrr",
@@ -42,7 +81,7 @@ def initVariables():
             "pibbbbip",
             "piiiiiip",
             "pppppppp"],
-        [   "pppppppp",
+        ["pppppppp",
             "vvvvvvvv",
             "pppppppp",
             "vvvvvvvv",
@@ -50,7 +89,7 @@ def initVariables():
             "vvvvvvvv",
             "pppppppp",
             "vvvvvvvv"],
-        [   "bbbbbbbb",
+        ["bbbbbbbb",
             "bbbbbbbb",
             "bbbbbbbb",
             "bbbbbbbb",
@@ -74,7 +113,7 @@ def initVariables():
             "roygbvpw",
             "roygbvpw",
             "roygbvpw"],
-        [   "rrrrrrrr",
+        ["rrrrrrrr",
             "oooooooo",
             "yyyyyyyy",
             "gggggggg",
@@ -92,22 +131,27 @@ def initVariables():
             "wwwwwwww"],
         ["00ooo000",
             "0o000o00",
-            "o0w0w0o0",
+            "o0g0g0o0",
             "o00000o0",
             "o0g0g0o0",
             "0o0g0o00",
             "00ooo000",
             "00000000"]]
 pic: List[str] = []
+messageRx = False
 picsB: List[List[str]] = []
+lastSentString = ""
+lastSentValue = 0
 picsA: List[List[str]] = []
-modes: List[str] = []
+lastSentName = ""
+ACK = False
 currentMode = ""
+modes: List[str] = []
 initVariables()
 radio.set_group(1)
+radio.send_string("ledMapTx")
 radio.send_string("updateLeds")
 basic.show_string("" + (currentMode))
-radio.send_string("ledMapTx")
 music.set_volume(50)
 music.set_tempo(500)
 
